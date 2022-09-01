02f0d966f1a9aba91143ca8d5c4916dd55eae1c9
Paulo Victor <paulovictor237@gmail.com>
2022-09-01 10:04:43 -0300

    swap

---
+++ scripts/settings.sh
@@ -0,0 +1,6 @@
+# increase the size of swapfile without removing
+sudo swapoff /swapfile
+sudo dd if=/dev/zero of=/swapfile bs=1G count=6 oflag=append conv=notrunc
+The above command will append 1GiB of zero bytes at the end of your swap file.
+sudo mkswap /swapfile
+sudo swapon /swapfile
