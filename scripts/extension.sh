5732917cedecab323995fb33ccb6ae3621b6c0ad
Paulo Victor <paulovictor237@gmail.com>
2022-09-22 12:40:35 -0300

    bkp

--- scripts/extension.sh
+++ scripts/extension.sh
@@ -1,6 +1,9 @@
 # gnome extensions
 sudo apt install gnome-shell-extensions
 
+# gnome tweak-tool
+sudo apt install gnome-tweak-tool
+
 # clipboard history
 https://extensions.gnome.org/extension/4839/clipboard-history/
 
