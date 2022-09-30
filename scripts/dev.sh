02537556e40bbed56f4c32fa29302d9da9c7cab5
Paulo Victor <paulovictor237@gmail.com>
2022-09-30 17:59:52 -0300

    docker compose

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -49,3 +49,8 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo docker run hello-world
+
+# docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
