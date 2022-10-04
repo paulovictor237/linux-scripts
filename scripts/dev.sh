fbf215cb0493ae5a5acc1b3bfe7f2298779fa308
Paulo Victor <paulovictor237@gmail.com>
2022-10-04 08:32:43 -0300

    make docker as sudo group

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -50,6 +50,12 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo docker run hello-world
 
+# docker as sudo group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
+
 # docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
