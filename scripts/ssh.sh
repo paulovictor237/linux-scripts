d1e134bb7faa9383514a2b9c747bd20175cf998a
Paulo Victor <paulovictor237@gmail.com>
2022-12-10 16:40:38 -0300

    MULTIPLE SSH KEYS

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,20 +1,57 @@
-# ssh-keygen # SSH Generator
-# dir .ssh # List the contents of ~/.ssh to view the key files
-# eval $(ssh-agent) # start it manually ssh server
-# ssh-add ~/.ssh/id_rsa # Load your new identity into the ssh-agent
-# ssh-add -l # list the keys that the agent is managing.
-# more ~/.ssh/id_rsa.pub # (cat/tail -f)Show the key
-# ssh -T git@bitbucket.org #verify the key
-
 # Git Configuration
 git config --global user.name "peve"
 git config --global user.email "paulovictor237@gmail.com"
 
-# Set up SSH for Git
+# @@@ Set up SSH for Git @@@
+ssh-add -D && rm -rf ~/.ssh  # limpa tudo e reseta o ssh
 ssh-keygen # SSH Generator
-ls -a ~/.ssh # List the contents of ~/.ssh to view the key files
-ssh-agent /bin/bash # start it manually ssh server
-ps -e | grep [s]sh-agent # Start the ssh-agent and load your keys
 ssh-add ~/.ssh/id_rsa # Load your new identity into the ssh-agent
-ssh-add -l # list the keys that the agent is managing.
-more ~/.ssh/id_rsa.pub # (cat/tail -f)Show the key
+more ~/.ssh/*.pub # (cat/tail -f)Show the key
+ssh -T git@bitbucket.org #  response YES
+
+# @@@ MULTIPLE SSH KEYS @@@
+https://blog.developer.atlassian.com/different-ssh-keys-multiple-bitbucket-accounts/
+https://gist.github.com/klodshanz/3d2f34992e1a78c9876e3be2faf8aa8e
+# clean all
+ssh-add -D && rm -rf ~/.ssh  # limpa tudo e reseta o ssh
+
+# create SSH Keys
+ssh-keygen -t rsa -C "paulovictor237@gmail.com"
+/home/peve/.ssh/id_rsa
+ssh-keygen -t rsa -C "paulovictor237@gmail.com"
+/home/peve/.ssh/id_rsa_bb
+ls -a ~/.ssh 
+
+# add the identities to SSH
+ssh-add -D 
+ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa_bb
+ssh-add -l
+
+# Create Config file
+gnome-text-editor ~/.ssh/config
+#user1 account
+Host gh
+HostName bitbucket.org
+User git
+IdentityFile ~/.ssh/id_rsa
+IdentitiesOnly yes
+
+#user2 account
+Host bb
+HostName bitbucket.org
+User git
+IdentityFile ~/.ssh/id_rsa_bb
+IdentitiesOnly yes
+
+# FINISH
+cat  ~/.ssh/id_rsa.pub
+cat  ~/.ssh/id_rsa_bb.pub
+
+# Check that repo recognizes keys:
+# ssh -T git@bitbucket.org #  response YES
+ssh -T gh
+ssh -T bb
+
+# git clone git@gh:etikett_de/relaunch.git
+# git clone git@bb:etikett_de/relaunch.git
