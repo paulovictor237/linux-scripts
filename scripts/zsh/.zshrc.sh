f13939566635b44c88a0f66b53fdc864cda85b15
peve <paulovictor237@gmail.com>
2023-02-06 10:54:18 -0300

    ANDROID_HOME

--- scripts/zsh/.zshrc.sh
+++ scripts/zsh/.zshrc.sh
@@ -28,4 +28,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+# export ANDROID_HOME=~/Android/Sdk
+# export PATH=$PATH:$ANDROID_HOME/emulator
+# export PATH=$PATH:$ANDROID_HOME/tools
+# export PATH=$PATH:$ANDROID_HOME/tools/bin
+# export PATH=$PATH:$ANDROID_HOME/platform-tools
 
