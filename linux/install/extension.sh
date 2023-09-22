5f2e95afc7729c41b0978d07f86049dec0a941bd
Paulo Victor Duarte <paulovictor237@gmail.com>
2023-09-22 10:34:09 -0300

    extensions reorder

--- linux/install/extension.sh
+++ linux/install/extension.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 array=(
-  https://extensions.gnome.org/extension/3193/blur-my-shell/
   https://extensions.gnome.org/extension/352/middle-click-to-close-in-overview/
   https://extensions.gnome.org/extension/906/sound-output-device-chooser/
-  https://extensions.gnome.org/extension/3933/toggle-night-light/
   https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/
-  https://extensions.gnome.org/extension/5278/pano/
+  https://extensions.gnome.org/extension/3933/toggle-night-light/
+  https://extensions.gnome.org/extension/943/workspace-scroll/
   https://extensions.gnome.org/extension/4033/x11-gestures/
-  https://extensions.gnome.org/extension/2857/maximize-to-workspace-with-history/
   https://extensions.gnome.org/extension/4506/simple-system-monitor/
+  https://extensions.gnome.org/extension/5278/pano/
+
+  https://extensions.gnome.org/extension/3193/blur-my-shell/
+  https://extensions.gnome.org/extension/2639/hide-minimized/
   https://extensions.gnome.org/extension/4158/gnome-40-ui-improvements/
   https://extensions.gnome.org/extension/3740/compiz-alike-magic-lamp-effect/
-  https://extensions.gnome.org/extension/4269/alphabetical-app-grid/
-  https://extensions.gnome.org/extension/943/workspace-scroll/
-  https://extensions.gnome.org/extension/19/user-themes/
-  # https://extensions.gnome.org/extension/2639/hide-minimized/
+  https://extensions.gnome.org/extension/2857/maximize-to-workspace-with-history/
+
+  # https://extensions.gnome.org/extension/19/user-themes/
+  # https://extensions.gnome.org/extension/4269/alphabetical-app-grid/
   # https://extensions.gnome.org/extension/5853/show-apps-at-top/
   # https://extensions.gnome.org/extension/4709/another-window-session-manager/
   # https://extensions.gnome.org/extension/1162/emoji-selector/
