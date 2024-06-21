c7eef17b9ec3e88ec5444376a35e6f6b34a83b0f
Paulo Victor Duarte <paulovictor237@gmail.com>
2024-06-21 18:43:56 -0300

    sdk

--- linux/install/dev.sh
+++ linux/install/dev.sh
@@ -12,9 +12,6 @@ npm install -g yarn appcenter-cli serverless
 # appcenter login
 # sls login
 
-# huskyrc
-cp linux/assets/.huskyrc ~/
-
 # java
 sudo apt-get update
 sudo apt-get install openjdk-17-jdk -y
@@ -23,9 +20,6 @@ readlink -f $(which java)
 
 # watchman
 sudo apt -y install watchman                         
-
-# export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 
 # AWS
 cd ~/Downloads
@@ -46,7 +40,18 @@ aws configure # IAM User Access Keys
 # https://developer.android.com/studio/emulator_archive
 # stable emulator version (33.1.24)
 # emulator -list-avds
-# emulator @Pixel_4_API_33  
+# emulator @Pixel_4_API_33
+
+# https://redirector.gvt1.com/edgedl/android/repository/emulator-linux_x64-11237101.zip
+# /home/peve/Android/Sdk/$HOME/Android/Sdk
+# crie uma cópia da pasta emulator
+# Descompacte o arquivo ZIP do emulador transferido por download. Mova o conteúdo para o diretório de instalação do SDK.
+# Cole o arquivo package.xml do diretório emulator_original no novo diretório emulator.
+# Altere a versão do emulador especificada no arquivo package.xml para a versão que você transferiu por download e quer usar. Para fazer isso, role a tela até a parte de baixo do arquivo package.xml e encontre o texto semelhante a este:
+# <revision><major>31</major><minor>1</minor><micro>4</micro></revision>
+# Especifique a versão do emulador que você transferiu por download e quer instalar.
+<revision><major>33</major><minor>1</minor><micro>24</micro></revision>
+January 16, 2019</license><localPackage path="emulator" obsolete="false"><type-details xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="ns5:genericDetailsType"/><revision><major>33</major><minor>1</minor><micro>24</micro></revision><display-name>Android Emulator</display-name><uses-license ref="android-sdk-license"/></localPackage></ns2:repository>
 
 # rodar temporariamente o servidor no browser
 # npx -y flipper-server@0.195.0
