73683e7d699ce7463caf5a9a5e8514820852c7ad
PeVe <paulo@px.center>
2024-10-07 14:08:55 -0300

    myip

--- linux/assets/.zshrc.sh
+++ linux/assets/.zshrc.sh
@@ -24,7 +24,7 @@ alias foo='echo "$(hostname)@$(whoami):$PWD"'
 alias aliaslist='grep "^alias" ~/.zshrc | cut -d "=" -f 1 | cut -d " " -f 2'
 alias node_modules_remove="find . -name "node_modules" -type d -prune -exec rm -rf '{}' +"
 alias update="sudo apt update;sudo apt upgrade"
-alias myip="ip -4 addr | grep inet"
+alias myip="ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}'"
 alias killport='f() { sudo kill -9 $(sudo lsof -t -i:$1) };f'
 alias zshrc="code ~/.zshrc"
 
