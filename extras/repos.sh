e49c3439a5b75e711c7a57ca5efa951acceaa736
peve <paulovictor237@gmail.com>
2025-05-23 11:39:40 -0300

    repos

---
+++ extras/repos.sh
@@ -0,0 +1,14 @@
+for repo in \
+"git@github.com:paulovictor237/BDD.git" \
+"git@github.com:paulovictor237/ponto-mais.git" \
+"git@github.com:paulovictor237/linux-scripts.git" \
+"git@github.com:paulovictor237/rename-components.git" \
+"git@github.com:px-center/px-api-docs.git" \
+"git@github.com:px-center/px-academy-core.git" \
+"git@github.com:px-center/px-academy-web.git" \
+"git@github.com:px-center/px-painel.git" \
+"git@github.com:px-center/px-torre-core.git" \
+"git@github.com:px-center/px-mobile-motorista.git"
+do
+  git clone "$repo"
+done
