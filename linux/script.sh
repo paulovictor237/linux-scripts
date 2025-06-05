0d7dc4afb3b5453c7f0bcbfb225b72696eb969a6
peve <paulovictor237@gmail.com>
2025-06-05 12:47:51 -0300

    teleport

--- linux/script.sh
+++ linux/script.sh
@@ -102,13 +102,8 @@ flatpak install flathub -y --noninteractive org.flameshot.Flameshot
 sudo apt install libxtst-dev libxss-dev libprocps-dev libopencv-dev
 flatpak install flathub -y --noninteractive com.obsproject.Studio
 
-# install warp-terminal
-sudo apt-get install wget gpg
-wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdotdev.gpg
-sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt/keyrings/warpdotdev.gpg
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt/sources.list.d/warpdotdev.list'
-rm warpdotdev.gpg
-sudo apt update && sudo apt install warp-terminal
+# teleport
+curl https://goteleport.com/static/install.sh | bash -s 16.4.15
 
 # swap expand 19G
 free -m
