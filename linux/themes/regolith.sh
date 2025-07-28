ae1a793cdd3a0c339a75cc88db08d3246c4499b5
peve <paulovictor237@gmail.com>
2025-07-28 16:00:33 -0300

    Enhance regolith.sh: add wallpaper links and reorganize terminal tools installation; include dark mode fix for GTK apps

--- linux/themes/regolith.sh
+++ linux/themes/regolith.sh
@@ -12,16 +12,37 @@ sudo apt update
 sudo apt install regolith-desktop regolith-session-flashback regolith-look-lascaille
 # System Restart
 
-# Regolith Tweaks
-sudo apt install gnome-tweaks -y
+# fix dark mode in gtk apps
+mkdir -p ~/.config/xdg-desktop-portal
+echo -e "[preferred]\ndefault=gtk;wlr" > ~/.config/xdg-desktop-portal/portals.conf
 
 # terminal tools
 sudo apt install neofetch -y && neofetch
 sudo apt install btop -y && btop
 sudo apt install cmatrix -y && cmatrix
 sudo apt install cowsay fortune -y && fortune | cowsay
+sudo apt install bat -y && bat --version
+sudo apt install gnome-tweaks -y
 
-# fix dark mode in gtk apps
-mkdir -p ~/.config/xdg-desktop-portal
-echo -e "[preferred]\ndefault=gtk;wlr" > ~/.config/xdg-desktop-portal/portals.conf
+# Wallpapers
+https://wallhaven.cc/w/1jrkjv
+https://wallhaven.cc/w/v9owd3
+https://wallhaven.cc/w/5yd6d5
+https://wallhaven.cc/w/ly93ry
+https://www.reddit.com/r/unixporn/
+https://wall.alphacoders.com/big.php?i=1328375
+https://github.com/dharmx/walls/blob/main/apocalypse/a_motorcycle_parked_outside_a_restaurant.jpg
+https://github.com/notAxon/wallpapers/blob/main/Space/gruv-ixion.jpg
+https://github.com/notAxon/wallpapers/blob/main/catppuccin/wallhaven-d6j79o.png
+
+# 🛠️ Settings
+# https://regolith-desktop.com/docs/using-regolith/configuration/
+# ~/.config/regolith3/Xresources
+# Super + Alt + L
+
+
+
+
+
+
 
