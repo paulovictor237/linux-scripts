9b85834151639f53cb4716f7b920f79d458acf1e
Paulo Victor Duarte <42382188+paulovictor237@users.noreply.github.com>
2025-08-24 17:18:57 -0300

    macos

--- mac/ventura/install.sh
+++
@@ -1,7 +0,0 @@
-./install.sh -m -t all -l -N stable --normal --round
-sudo ./tweaks.sh -g 
-# .icons
-# https://github.com/vinceliuice/WhiteSur-gtk-theme
-# https://github.com/USBA/Cupertino-Ventura-iCons
-# https://www.gizmochina.com/2022/09/13/download-macos-13-ventura-wallpaper/
-# https://github.com/ful1e5/apple_cursor
